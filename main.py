c3328bee4473c417fe787e1579be14eda6dd7c77
Vyas Ashutosh <vyasaashutosh@gmail.com>
2022-10-30 00:03:14 +0530

    Try commit through VSCode W10.

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ print("Edited to add development branch on Windows.")
 print("Pushing commit through VSCodium rather than GitHub.")
 
 print("Adding another arbitrary print statement.")
+
+"""
+Adding some text as docstring.
+"""
