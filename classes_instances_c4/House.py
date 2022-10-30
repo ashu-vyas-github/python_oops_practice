f8e6eae94277ac5c71a560b7203c2fb7a73dcd41
Vyas Ashutosh <vyasaashutosh@gmail.com>
2022-10-31 00:48:33 +0530

    Begin OOP concepts practice with simple class definition.

---
+++ classes_instances_c4/House.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: ashutosh
+
+Class definition of House class used as a toy example for OOP concepts.
+
+The House module contains the House class used as toy example to practice object-
+oriented programming concepts. This will be updated throughout as new concepts are
+learned and practiced.
+
+OPTIONS ------------------------------------------------------------------
+A description of each option that can be passed to this script.
+(Placeholder for now, will be updated later.)
+
+ARGUMENTS -------------------------------------------------------------
+A description of each argument that can or must be passed to this script.
+(Placeholder for now, will be updated later.)
+"""
+
+
+class House:
+    pass
