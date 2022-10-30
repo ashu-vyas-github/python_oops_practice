519e4ebb125d11afe713e3353d55dd559db4b0de
Vyas Ashutosh <vyasaashutosh@gmail.com>
2022-10-31 00:55:55 +0530

    Add init method for object initialization.

--- classes_instances_c4/House.py
+++ classes_instances_c4/House.py
@@ -21,4 +21,32 @@ A description of each argument that can or must be passed to this script.
 
 
 class House:
-    pass
+    """
+    Class definition of House class used as a toy example for OOP concepts.
+
+    The House module contains the House class used as toy example to practice object-
+    oriented programming concepts. This will be updated throughout as new concepts are
+    learned and practiced.
+
+    OPTIONS ------------------------------------------------------------------
+    A description of each option that can be passed to this script.
+    (Placeholder for now, will be updated later.)
+
+    ARGUMENTS -------------------------------------------------------------
+    A description of each argument that can or must be passed to this script.
+    (Placeholder for now, will be updated later.)
+    """
+
+    def __init__(self, price):
+        """
+        House class constructor method for object initialization.
+
+        Constructor initialization method used and called upon creation of House object.
+
+        Parameters
+        ----------
+        price : float
+            Price of the house with decimal places.
+        """
+        self.price = price
+        pass
