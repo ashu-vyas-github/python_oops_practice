4f6afd32bc7f87846f77e816f14eb4344b14e512
Vyas Ashutosh <vyasaashutosh@gmail.com>
2022-10-31 22:56:16 +0530

    Create exercise file.

---
+++ classes_instances_c4/Exercise_Bugfix.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: ashutosh
+
+Mini project of bugs fixing exercise.
+
+Resolve the typos and bugs in the below example:
+
+class Donut
+        def _ini__(flavor, toppings, filling, size):
+                self.flavor = flavor
+                self.toppings = toppings
+                self.filling = filling
+                size = self.size
+
+class Customer:
+
+	def _init_(self, name, age address, favorite_dessert)
+		self.name = name
+		self.age = age
+		self.address = address
+		favorite_dessert = self.favorite_dessert
+
+Cake:
+	__init__(self, flavor, price, quality):
+		self.flavor = flavor
+		self.price = price
+		self.quality = quality
+
+"""
