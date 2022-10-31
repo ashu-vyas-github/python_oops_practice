b570a43a63134cac884aa5c73d16fe601485a57e
Vyas Ashutosh <vyasaashutosh@gmail.com>
2022-10-31 19:05:21 +0530

    Create Movie class example.

---
+++ classes_instances_c4/Movie.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: ashutosh
+
+Class definition of Movie class used as a toy example for OOP concepts.
+
+The Movie module contains the Movie class used as toy example to practice object-
+oriented programming concepts. This will be updated throughout as new concepts are
+learned and practiced.
+
+OPTIONS ------------------------------------------------------------------
+A description of each option that can be passed to this script.
+(Placeholder for now, will be updated later.)
+
+ARGUMENTS -------------------------------------------------------------
+A description of each argument that can or must be passed to this script.
+(Placeholder for now, will be updated later.)
+"""
+
+
+class Movie:
+    """
+    Class definition of Movie class used as a toy example for OOP concepts.
+
+    The Movie module contains the Movie class used as toy example to practice object-
+    oriented programming concepts. This will be updated throughout as new concepts are
+    learned and practiced.
+
+    OPTIONS ------------------------------------------------------------------
+    A description of each option that can be passed to this script.
+    (Placeholder for now, will be updated later.)
+
+    ARGUMENTS -------------------------------------------------------------
+    A description of each argument that can or must be passed to this script.
+    (Placeholder for now, will be updated later.)
+    """
+
+    def __init__(self, title, year, language, rating):
+        """
+        Movie class constructor method for object initialization.
+
+        Constructor initialization method used and called upon creation of Movie object.
+
+        Parameters
+        ----------
+        title : String
+            Title of the movie to be initialized.
+            Eg: title="The Godfather"
+        year : Int
+            Year when the movie was released for general public in theaters.
+            Must be positive valued number.
+            Eg: year=1972
+        language : String
+            Original language in which the movie was released initially for general public.
+            Eg: language="English"
+        rating : Int
+            User-rating of the movie between 0 and 10.
+            Eg: rating=9
+        """
+        self.title = title
+        self.year = year
+        self.language = language
+        self.rating = rating
