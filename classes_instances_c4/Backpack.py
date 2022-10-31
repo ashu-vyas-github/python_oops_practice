04384b460fbd6953c71eba890890a1df5039b702
Vyas Ashutosh <vyasaashutosh@gmail.com>
2022-10-31 18:34:00 +0530

    Create Backpack class example with items list.

---
+++ classes_instances_c4/Backpack.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: ashutosh
+
+Class definition of Backpack class used as a toy example for OOP concepts.
+
+The Backpack module contains the Backpack class used as toy example to practice object-
+oriented programming concepts. This will be updated throughout as new concepts are
+learned and practiced.
+
+OPTIONS ------------------------------------------------------------------
+A description of each option that can be passed to this script.
+(Placeholder for now, will be updated later.)
+
+ARGUMENTS -------------------------------------------------------------
+A description of each argument that can or must be passed to this script.
+(Placeholder for now, will be updated later.)
+"""
+
+
+class Backpack:
+    """
+    Class definition of Backpack class used as a toy example for OOP concepts.
+
+    The Backpack module contains the Backpack class used as toy example to practice object-
+    oriented programming concepts. This will be updated throughout as new concepts are
+    learned and practiced.
+
+    OPTIONS ------------------------------------------------------------------
+    A description of each option that can be passed to this script.
+    (Placeholder for now, will be updated later.)
+
+    ARGUMENTS -------------------------------------------------------------
+    A description of each argument that can or must be passed to this script.
+    (Placeholder for now, will be updated later.)
+    """
+
+    def __init__(self, items):
+        """
+        Backpack class constructor method for object initialization.
+
+        Constructor initialization method used and called upon creation of Backpack object.
+
+        Parameters
+        ----------
+        items : list
+            Items that are contained in the backpack. Initially, it is empty.
+        """
+        self.items = []
+
