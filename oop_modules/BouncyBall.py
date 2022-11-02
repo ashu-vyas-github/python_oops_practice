0ed6ab9fe1392d37dc0c3fe53f33874471d5ef3b
Vyas Ashutosh <vyasaashutosh@gmail.com>
2022-11-03 03:44:32 +0530

    Exercise on getter, setter, property, decorator.

---
+++ oop_modules/BouncyBall.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: ashutosh
+
+Exercise example for getter, setter, property, decorator.
+"""
+
+
+class BouncyBall:
+    def __init__(self, price, size, brand):
+        self.price = price
+        self.size = size
+        self.brand = brand
